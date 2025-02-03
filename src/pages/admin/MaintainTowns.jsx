245d2db7b1745e6d7b092ea057ccbdd040dd5294
KT147 <kevin.tammm@gmail.com>
2025-02-03 21:27:09 +0200

    .

--- src/pages/admin/MaintainTowns.jsx
+++ src/pages/admin/MaintainTowns.jsx
@@ -1,21 +1,32 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import townsJSON from "../../data/shops.json"
+import { Link } from "react-router-dom"
 
 function MaintainTowns() {
+    const [towns, setTowns] = useState([...townsJSON.filter(shop => shop.town)])
 
-    const [towns, setTowns] = useState(townsJSON)
+    const townRef = useRef()
 
     const deleteTown = (index) => {
-        townsJSON[index].town= ""
+        townsJSON.splice(index, 1)
         setTowns(townsJSON.slice())
     }
+
+    const updateTown = (index, newTownName) => {
+        const updatedTowns = [...towns]
+        updatedTowns[index].town = newTownName
+        setTowns(updatedTowns)
+}
+
     return (
     <div>
-        {towns.map((town, index)=>
-        
-            <div key={town}>
-                <div>{town.town}</div>
-                <button onClick={() => deleteTown (index)} className="button">Delete</button>
+        {towns.map((town, index) =>
+            <div key={town.lat}>
+                <input defaultValue={town.town} ref={townRef} type="text" onChange={(e) => updateTown(index, e.target.value)}  /> <br />
+                <Link to="/admin/maintain-shops">
+                    <button className="button">Edit</button>
+                </Link>
+                <button onClick={() => deleteTown(index)} className="button">Delete</button>
                 <br /><br />
             </div>)}
     </div>
